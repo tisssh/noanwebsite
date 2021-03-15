6a388284d788bab2a48a15a38d42ef88fe1f7146
tisssh <79740639+tisssh@users.noreply.github.com>
2021-03-15 23:00:56 +0900

    init

---
+++ scripts/libs/hero-slider.js
@@ -0,0 +1,37 @@
+class HeroSlider {
+    constructor(el) {
+        this.el = el;
+        this.swiper = this._initSwiper();
+    }
+
+    _initSwiper() {
+        return new Swiper(this.el, {
+            // Optional parameters
+            // direction: 'vertical',
+            loop: true,
+            grabCursor: true,
+            effect: 'coverflow',
+            centeredSlides: true,
+            slidesPerView: 1,
+            speed: 1800,
+            breakpoints: {
+                1024: {
+                    slidesPerView: 2,
+                }
+            },
+        });
+    }
+
+    start(options = {}) {
+        options = Object.assign({
+            delay: 5000,
+            disableOnInteraction: false
+        }, options);
+        
+        this.swiper.params.autoplay = options;
+        this.swiper.autoplay.start();
+    }
+    stop() {
+        this.swiper.autoplay.stop();
+    }
+}
